c4874c5eeda382b24ab4b2d2944156ff1fb67beb
Eliza-Hofer <liz.m.hofer@gmail.com>
2024-04-07 20:51:08 -0400

    Facebook event grabber
    I started the foundations of getting a facebook event grabber. I will need to create a facebook account and an app

--- index.js
+++ index.js
@@ -18,3 +18,39 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+// Function to fetch Facebook events
+function fetchFacebookEvents() {
+    var pageId = "YOUR_PAGE_ID";
+    var accessToken = "YOUR_ACCESS_TOKEN";
+    var apiUrl = "https://graph.facebook.com/v12.0/" + pageId + "/events?access_token=" + accessToken;
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        displayEvents(data.data);
+    })
+    .catch(error => {
+        console.error('Error fetching events:', error);
+    });
+}
+
+// Function to display events on the webpage
+function displayEvents(events) {
+    var eventsList = document.getElementById("events-list");
+    eventsList.innerHTML = "";
+
+    events.forEach(event => {
+        var eventName = event.name;
+        var eventDate = new Date(event.start_time).toLocaleString();
+        var eventLink = event.link;
+
+        var eventElement = document.createElement("div");
+        eventElement.innerHTML = "<h3>" + eventName + "</h3><p>Date: " + eventDate + "</p><p><a href='" + eventLink + "' target='_blank'>Event Link</a></p>";
+
+        eventsList.appendChild(eventElement);
+    });
+}
+
+// Call the fetchFacebookEvents function when the page loads
+window.onload = fetchFacebookEvents;
